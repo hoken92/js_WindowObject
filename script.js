4630f5eca37d9b63fda33f57b24e78bd34ec217d
hoken92 <hkenneth92@gmail.com>
2024-09-26 20:41:51 -0700

    number game

---
+++ script.js
@@ -0,0 +1,44 @@
+// Window asking user a guessing game
+
+// DOM elements
+const divEl = document.getElementById("app");
+const bodyEl = document.body;
+
+// Random number
+const number = Math.floor(Math.random() * 5).toString();
+
+// Game Round number
+let count = 0;
+
+// Loops the guess the number game 3 times
+function loopGuessNumber() {
+  // Pops a modal for user to guess the number
+  let userInput = prompt("What number am I thinking of?");
+
+  // If the number is correct, exits the game and shows the answer
+  if (userInput === number) {
+    bodyEl.style.backgroundColor = "green";
+    bodyEl.textContent = `You got it right! The number was ${number}`;
+    return;
+  } else if (userInput > number) {
+    bodyEl.style.backgroundColor = "red";
+    bodyEl.textContent = `Your number is too high. Try again`;
+  } else {
+    bodyEl.style.backgroundColor = "red";
+    bodyEl.textContent = `Your number is too low. Try again`;
+  }
+  // increase the game round count by 1
+  count++;
+
+  // If count is less than 3, delay the next round by 3 seconds
+  if (count < 3) {
+    setTimeout(loop, 3000);
+  }
+
+  // If count is = 3, game over
+  if (count === 3) {
+    bodyEl.textContent = `Game over. The number was ${number}`;
+  }
+}
+
+loopGuessNumber();
